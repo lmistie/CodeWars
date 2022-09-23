e3a3c313f1c30c2f3eb4fa57b0f1ea097157cccc
lmistie <romaniy11052001@gmail.com>
2022-09-23 15:49:27 +0300

    NOTbst c++

--- Codewars_C++/7kyu/NotBST/NotBST.cpp
+++ Codewars_C++/7kyu/NotBST/NotBST.cpp
@@ -1,18 +1,40 @@
 #include "iostream"
+#include "vector"
+#include "cstddef"
 
 struct Node{
-    int val;
-    Node *left = nullptr;
-    Node *right = nullptr;
+int val;
+Node *left, *right = NULL;
 };
 
 bool search(int n, Node *root){
-    if(root == nullptr) return false;
-    if(root->val == n) return true;
-    if(root->val > n) return search(n, root->left);
-    return search(n, root->right);
+
+if (!root) {
+return false;
+}
+else{
+if (root->val == n) {
+return true;
+}
+}
+return (search(n, root->right) || search(n, root->left));
 }
 
-int main() {
-    
+int main () {
+    Node *root = new Node();
+    root->val = 5;
+    root->left = new Node();
+    root->left->val = 3;
+    root->right = new Node();
+    root->right->val = 7;
+    root->left->left = new Node();
+    root->left->left->val = 2;
+    root->left->right = new Node();
+    root->left->right->val = 4;
+    root->right->left = new Node();
+    root->right->left->val = 6;
+    root->right->right = new Node();
+    root->right->right->val = 8;
+    std::cout << search(3, root) << std::endl;
+    return 0;
 }
